e808bf9c6b7ab97f0dee828f77f49a4135b443da
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-08 16:52:42 -0300

    Update plugin

---
+++ src/Plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * Date: 08/06/2018
+ * Time: 16:46
+ */
+
+namespace JeffersonSimaoGoncalves\ApiPagination;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Class Plugin
+ *
+ * @author Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
+ *
+ * @package JeffersonSimaoGoncalves\ApiPagination
+ */
+class Plugin extends BasePlugin
+{
+
+}
